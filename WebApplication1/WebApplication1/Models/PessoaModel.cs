1f796d0c86e22a76a2360c8b14262025fc54e35e
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2023-06-15 10:07:51 -0300

    Add para publish test

---
+++ WebApplication1/WebApplication1/Models/PessoaModel.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Models
+{
+    public class PessoaModel
+    {
+        private int v;
+        private object value;
+
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string celular { get; set; }
+
+
+
+         public PessoaModel(int id, string nome, string email, string celular)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.email = email;
+            this.celular = celular;
+        }
+
+    }
+}
